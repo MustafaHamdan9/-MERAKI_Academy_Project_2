294243d8475c24bf17668b42eaf4357e2597dfbe
MustafaHamdan9 <mmhamdan991@gmail.com>
2021-04-28 17:02:31 +0300

    build the function

--- main.js
+++ main.js
@@ -1,1 +1,71 @@
-console.log("hello");
+const body = $("body");
+const main = $(".main");
+body.append(main);
+
+////////////////////////////////////
+
+const europe = $(".eur");
+main.append(europe);
+
+const italy = $(".italy");
+europe.append(italy);
+
+const french = $(".french");
+europe.append(french);
+
+const spain = $(".spain");
+europe.append(spain);
+
+const british = $(".british");
+europe.append(british);
+
+////////////////////////////////////
+
+const asia = $(".asia");
+main.append(asia);
+
+const china = $(".china");
+asia.append(china);
+
+const Midest = $(".Midest");
+asia.append(Midest);
+
+const Korea = $(".Korea");
+asia.append(Korea);
+
+const japan = $(".japan");
+asia.append(japan);
+
+////////////////////////////////////
+
+const africa = $(".africa");
+main.append(africa);
+
+const moroco = $(".moroco");
+africa.append(moroco);
+
+const algeria = $(".algeria");
+africa.append(algeria);
+
+const sudan = $(".sudan");
+africa.append(sudan);
+
+const egypt = $(".egypt");
+africa.append(egypt);
+
+////////////////////////////////////
+
+const america = $(".amerca");
+main.append(america);
+
+const usa = $(".usa");
+america.append(usa);
+
+const canda = $(".canda");
+america.append(canda);
+
+const brazil = $(".brazil");
+america.append(brazil);
+
+const argentina = $(".argin");
+america.append(argin);
